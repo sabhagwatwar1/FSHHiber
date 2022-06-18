d780a3df9332163a685a3f2b7e52fcd661e02d17
Ram <sabhagwatwar@gmail.com>
2022-06-18 09:37:50 +0530

    final commit

--- src/main/java/com/hib/HibernateSec/App.java
+++ src/main/java/com/hib/HibernateSec/App.java
@@ -1,5 +1,10 @@
 package com.hib.HibernateSec;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project started" );
+        Configuration cfg=new Configuration();
+        cfg.configure();
+        SessionFactory factory=cfg.buildSessionFactory();
+        Session s=factory.openSession();
+        
+        Student st=new Student(101,"Ram","Mumbai");
+        Transaction t=s.beginTransaction();
+        
+        s.save(st);
+        t.commit();
+        
+        s.close();
+        factory.close();
     }
 }
